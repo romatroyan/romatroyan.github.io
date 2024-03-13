aa952be137ac1819309da0946f20817733d7c3d6
romatroyan12 <romatroyann@gmail.com>
2024-03-13 16:01:25 +0300

    none

--- js/views/pages/home.js
+++ js/views/pages/home.js
@@ -1,13 +1,40 @@
 import Component from '../component.js';
 
+
 class Home extends Component {
+
+   
     render() {
         return new Promise(resolve => {
             resolve(`
-                
+            <div class="container-slider">
+            <div class="slider">
+                <div><img src="../../../img/sliderHome/1.jpg" alt=""></div>
+                <div><img src="../../../img/sliderHome/2.jpg" alt=""></div>
+                <div><img src="../../../img/sliderHome/3.jpg" alt=""></div>
+            </div>
+            </div>
             `);
         });
     }
+
+    slider(){
+        $('.slider').slick({
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+        });
+       };
+
+    afterRender() {
+        this.slider();
+    }
+
+    
+    
 }
+    
+
 
 export default Home;
