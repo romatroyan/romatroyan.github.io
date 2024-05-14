0be24cfbfb657f5fcb54c688dbf15b6aaf1dbc75
romatroyan12 <romatroyann@gmail.com>
2024-05-14 20:38:01 +0300

    none

--- js/views/pages/mapkleck.js
+++ js/views/pages/mapkleck.js
@@ -5,7 +5,7 @@ class Mapkleck extends Component {
         return new Promise(resolve => {
             resolve(`
                             
-            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A9fbc037b4d18604b6ed699d6ff57e071664de77d7d28ecd8ff922e450aaa33ef&amp;source=constructor" width="648" height="444" frameborder="0"></iframe>        
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A9fbc037b4d18604b6ed699d6ff57e071664de77d7d28ecd8ff922e450aaa33ef&amp;source=constructor" width="1140" height="650" frameborder="0"></iframe>   
             `);
         });
     }
